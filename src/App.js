65f8c3957ceafb94aac49d1d05befc5117d78ef2
Debdeep-Guha27 <lioneldeb27@gmail.com>
2025-03-09 17:04:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import About from './pages/About';
 import Features from './pages/Features';
 import ChooseUs from './pages/ChooseUs';
 import Mock_interview from './pages/Mock_interview';
+import DifficultyLevel from './pages/DifficultyLevel';
+import Allow from './pages/Allow';
+import Interview_chatbox from './pages/Interview_chatbox';
+import Form from './components/Form';
+import Chatbox from './components/Chatbox';
 function App() {
   return (
     <div className="App">
@@ -19,6 +24,11 @@ function App() {
         <Route path = '/features' element={<Features/>}/>
         <Route path = '/chooseus' element={<ChooseUs/>}/>
         <Route path = '/mockinterview' element={<Mock_interview/>}/>
+        <Route path = '/difficulty' element={<DifficultyLevel/>}/>
+        <Route path = '/allow' element={<Allow/>}/>
+        <Route path = '/interviewchatbox' element={<Interview_chatbox/>}/>
+        <Route path = '/form' element={<Form/>}/>
+        <Route path = '/chatbox' element={<Chatbox/>}/>
       </Routes>
     </div>
   );
